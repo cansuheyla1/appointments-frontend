d837762dd998949786a129d6478e178778cecd14
cansuheyla1 <cansuheyla8@gmail.com>
2026-02-09 23:29:14 +0300

    set backend base url for render

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://appointments-backend-ae8k.onrender.com/"
 })
 
 export default api
