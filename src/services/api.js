582eafd38ecd719847d2483fdbe8c89e87440df6
cansuheyla1 <cansuheyla8@gmail.com>
2026-02-11 23:37:49 +0300

    Update API base URL to include '/api' path

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://appointments-backend-ae8k.onrender.com"
+    baseURL: "https://appointments-backend-ae8k.onrender.com/api"
 })
 
 export default api
